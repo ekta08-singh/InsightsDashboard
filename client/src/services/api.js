f360694950857d5652f6f1b46b9f6f513e5d51c6
ekta <ekta08-singh>
2025-12-30 19:46:27 +0530

    Prepare frontend for Render deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getUser } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
